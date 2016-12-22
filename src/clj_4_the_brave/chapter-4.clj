1febe396a0cc63b588ede576b34f53dff1eceeb4
pRADeep <pradeepcheers@gmail.com>
2016-12-22 23:53:02 +0000

    resolved merge conflict

--- src/clj_4_the_brave/chapter-4.clj
+++ src/clj_4_the_brave/chapter-4.clj
@@ -25,8 +25,62 @@
 
 ;; when it comes to using seq functions on a data structure, all Clojure asks is “can I first, rest, and cons it?” If the answer is yes, you can use the seq library with that data structure
 
+(ns clj-4-the-brave.core)
+
+;; Indirection is a generic term for the mechanism a language employs so that one name can have multiple related meanings. Ploymorphism is one way clojure provides indirection.
+
+;; Whenever Clojure expects a sequence - Eg; when you call map, first, rest or cons it calls the seq function on the data structure in question to obtain a data structure that allows for first, rest and cons. Seq function always reutrns a value that looks and behaves like a list. Seq of maps consists of two elements key-value vectors. Thats why map teats your maps like lists of vector.
+
+(seq {:a 1 :b 2})
+
+(into {} (seq {:a 1 :b 2}))
+
+;; the sequence functions are defined in terms of sequence abstraction using first, rest and cons. As long as the datastructures implements sequence abstraction, it can use the extensive seq library which includes super function such as reduce, filter, distinct, groub-by and much more.
+
+;; The take away here is that its powerful to focus on what we can do with a data structure and to ignore the implemenataion as much as possible.
+
+;; In general programming to abstractions gives you power by letting you use libraries of functions on different data structres regardless of how those function are implemented.
+
+;; Map function
+
+(map inc [1 2 3])
+
 
 (map str ["a" "b" "c"] ["A" "B" "C"])
+
+(map str ["a" "b" "c"] ["A" "B"])
+
+
+;; when you pass map multiple collections, the element of the first collection will be passes as the first argument of the mapping function (str), the element of the second collection will be passesd as a second argument to the mapping function and so on. Just to be sure that the number of arguments of mapping function is equal to number of collection you are passing to the map.
+
+(def human-consumption [8.1 7.3 3.4 5.9])
+(def critter-consumption [0.0 0.2 0.3 1.1])
+
+(defn unify-diet-data 
+  [human critter]
+  {:human human :critter critter})
+
+(map unify-diet-data human-consumption critter-consumption)
+
+;; Another fun thing you can do with map is pass it a collection of functions. You could use this if you wanted to perform a set of calculations on different collections of numbers.
+
+(def sum #(reduce + %))
+(def avg #(/ (sum %) (count %)))
+
+(defn stats
+  [numbers]
+  (map #(% numbers) [sum count avg]))
+
+(stats [3 4 10])
+(stats [80 1 44 13 6])
+
+(def identities
+   [{:alias "Batman" :real "Bruce Wayne"}
+    {:alias "Spider-Man" :real "Peter Parker"}
+    {:alias "Santa" :real "Your mom"}
+    {:alias "Easter Bunny" :real "Your dad"}])
+
+(map :real identities)
 
 ;; Reduce
 
@@ -144,6 +198,56 @@
 
 
 
+
+
+;;**** Add work buffer changes  ****
+
+;; Infinite Sequence
+
+(concat (take 8 (repeat "na")) ["Bat-man!"])
+
+;; repeat creates a sequence whose every member is the argument you pass
+
+(take 8 (repeat "na"))
+
+;; repeatedly which will call the provided function to generate each element in the sequence
+(take 3 (repeatedly (fn [] (rand-int 10))))
+
+(defn even-numbers
+  ([] (even-numbers 0))
+  ([n] (cons n (lazy-seq (even-numbers (+ n 2))))))
+
+(take 10 (even-numbers))
+
+(cons 0 '(2 4 6))
+
+;; lisp programmers call it consing when they use cons function
+
+;; The Collection Abstraction, the collection abstraction is closely related to the sequence abstraction. All of Clojure’s core data structures—vectors, maps, lists, and sets—take part in both abstractions.
+
+;; The sequence abstraction is about operating on members individually, whereas the collection abstraction is about the data structure as a whole. For example, the collection functions count, empty?, and every? aren’t about any individual element; they’re about the whole
+
+(empty? [])
+
+(empty? [1 2 3])
+
+;; Lazy Sequence, Infinite Sequence, Sequece Avstraction, Collection Abstraction
+
+;; into: One of the most important collection functions is into. As you now know, many seq functions return a seq rather than the original data structure. You’ll probably want to convert the return value back into the original value, and into lets you do that
+
+(map identity {:sunlight-reaction "Glitter!"})
+
+;; the map function here returns the sequential data structure after being given a map data structure and into converts sequence back into map
+
+(into {} (map identity {:sunlight-reaction "Glitter!"}))
+
+(map identity [:garlic :sesame-oil :fried-eggs])
+
+(into [] (map identity [:garlic :sesame-oil :fried-eggs]))
+
+(map identity [:garlic-clove :garlic-clove])
+
+(into #{} (map identity [:garlic-clove :garlic-clove]))
 
 
 
